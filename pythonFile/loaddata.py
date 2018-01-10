ad9d11050facac1f9005737ba1ad4b71f5a1b021
mrinmoy sarkar <mrinmoy.pol@gmail.com>
2018-01-10 16:00:53 -0500

    train and test python code is added
    Signed-off-by: mrinmoy sarkar <mrinmoy.pol@gmail.com>

--- pythonFile/loaddata.py
+++ pythonFile/loaddata.py
@@ -10,108 +10,63 @@ import sqlite3
 import matplotlib.pyplot as plt
 from datetime import datetime
 import math
+import numpy as np
 
 def load_data(file_path,table_name):
-    con = sqlite3.connect(filePath)
+    con = sqlite3.connect(file_path)
     df = pd.read_sql_query("SELECT * from " + table_name, con)
     con.close()
     return df
 
 
 if __name__ == "__main__":
-    file_path = '../../2017-12-04_Original_scenarios_remix/1.sqlite3'
-    table_name = "mavlink_attitude"
-    attitude_df = load_data(file_path, table_name)
-    
-    table_name = "mavlink_scaled_imu2"
-    imu_df = load_data(file_path, table_name)
-    
-    table_name = "system_state"
-    state_df = load_data(file_path,table_name)
-    
-    #plt.plot(acce['xacc'],color='green',linewidth=3,label='hello',marker='^')
-    #plt.legend()
-    #plt.show()
-    
-#    #attitude_df.plot.hist()
-#    xacc = acce_df['time_boot_ms']#['xacc']
-#    z=xacc.mean()
-#    time_stamp = attitude_df['time_boot_ms']
-#    
-#    #print(attitude_df.index[attitude_df['time_boot_ms']<=42079][-1])
-#    
-#    n = attitude_df.shape[0]
-#    class_label_start_indexes = [0]
-#    #print(attitude_df.describe())
-#    index1 = attitude_df.index[attitude_df['time_boot_ms']<42079][-1]
-#    states = ["Hold"]*index1
-#    class_label_start_indexes += [index1+1]
-#    index2 = attitude_df.index[attitude_df['time_boot_ms']<76932][-1]
-#    states += ["Fly Orbit and Observe"]*(index2-index1)
-#    index1 = index2
-#    class_label_start_indexes += [index1+1]
-#    index2 = attitude_df.index[attitude_df['time_boot_ms']<222611][-1]
-#    states += ["Fly Search Pattern"]*(index2-index1)
-#    index1 = index2
-#    class_label_start_indexes += [index1+1]
-#    index2 = attitude_df.index[attitude_df['time_boot_ms']<294892][-1]
-#    states += ["Survey Target"]*(indx2-indx1)
-#    index1 = index2
-#    class_label_start_indexes += [index1+1]
-#    index2 = attitude_df.index[attitude_df['time_boot_ms']<414069][-1]
-#    states += ["Fly Orbit and Observe"]*(index2-index1+1)
-#    #indx1 = indx2
-#    #indx2 = attitude_df.index[attitude_df['time_boot_ms']>=414069][-1]
-#    #print(indx2)
-#    #states += ["Fly Orbit and Observe"]*(indx2-indx1)
-#    
-#    train_data_df = pd.concat([attitude_df,pd.DataFrame({"state":states})],axis=1)
-#    #acce_df = acce_df.append(acce_df.tail(1))
-#    train_data_df = pd.concat([train_data_df, acce_df],axis=1)
-#    
-#    
-#    str_date = "2017-12-04 21:08:48.897135"
-#    d_date1 = datetime.strptime(str_date , '%Y-%m-%d %H:%M:%S.%f')
-#    
-#    str_date = "2017-12-04 21:09:32.797271"
-#    d_date2 = datetime.strptime(str_date , '%Y-%m-%d %H:%M:%S.%f')
-#    d_date = math.ceil(d_date2.timestamp()*1000-d_date1.timestamp()*1000)
-#    
-#    
-#    print(d_date)
-    states = []
-    n = state_df.shape[0]
-    t0 = state_df['timestamp'][0]
-    t0 = datetime.strptime(t0 , '%Y-%m-%d %H:%M:%S.%f')
-    t0 = t0.timestamp()*1000
-    index1 = 0
-    all_states = state_df['state']
-    for i in range(n-1):
-        t = state_df['timestamp'][i+1]
-        t = datetime.strptime(t , '%Y-%m-%d %H:%M:%S.%f')
-        t = t.timestamp()*1000
-        dt = math.ceil(t-t0)
-        #print(dt)
-        index2 = attitude_df.index[attitude_df['time_boot_ms']<dt][-1]
-        states += [all_states[i]]*(index2-index1)
-        index1 = index2
         
-    train_data_df = pd.DataFrame({"state":states})
-    attitude_feature = ["roll", "pitch", "yaw", "rollspeed", "pitchspeed", "yawspeed"]
+    l1 = list(np.linspace(1,7,7,dtype=np.int)) + list(np.linspace(10,22,13,dtype = np.int))
+    ll = len(l1)
+    for l in range(ll):
+        file_path = '../../2017-12-04_Original_scenarios_remix/'+str(l1[l])+'.sqlite3'
+        table_name = "mavlink_attitude"
+        attitude_df = load_data(file_path, table_name)
+        
+        table_name = "mavlink_scaled_imu2"
+        imu_df = load_data(file_path, table_name)
     
-    n = len(attitude_feature)
-    for i in range(n):
-        train_data_df = pd.concat([train_data_df,pd.DataFrame({attitude_feature[i]:attitude_df[attitude_feature[i]]})],axis = 1)    
+        table_name = "system_state"
+        state_df = load_data(file_path,table_name)
+        states = []
+        n = state_df.shape[0]
+        t0 = state_df['timestamp'][0]
+        t0 = datetime.strptime(t0 , '%Y-%m-%d %H:%M:%S.%f')
+        t0 = t0.timestamp()*1000
+        index1 = 0
+        all_states = state_df['state']
+        for i in range(n-1):
+            t = state_df['timestamp'][i+1]
+            t = datetime.strptime(t , '%Y-%m-%d %H:%M:%S.%f')
+            t = t.timestamp()*1000
+            dt = math.ceil(t-t0)
+            #print(dt)
+            index2 = attitude_df.index[attitude_df['time_boot_ms']<dt][-1]
+            states += [all_states[i]]*(index2-index1)
+            index1 = index2
+        
+        train_data_df = pd.DataFrame({"state":states})
+        attitude_feature = ["roll", "pitch", "yaw", "rollspeed", "pitchspeed", "yawspeed"]
+    
+        n = len(attitude_feature)
+        for i in range(n):
+            train_data_df = pd.concat([train_data_df,pd.DataFrame({attitude_feature[i]:attitude_df[attitude_feature[i]]})],axis = 1)    
     
     
-    imu_feature = ["xacc", "yacc", "zacc"]
-    n = len(imu_feature)
-    for i in range(n):
-        train_data_df = pd.concat([train_data_df,pd.DataFrame({imu_feature[i]:imu_df[imu_feature[i]]})],axis = 1)
+        imu_feature = ["xacc", "yacc", "zacc"]
+        n = len(imu_feature)
+        for i in range(n):
+            train_data_df = pd.concat([train_data_df,pd.DataFrame({imu_feature[i]:imu_df[imu_feature[i]]})],axis = 1)
     
-    train_data_df.to_csv("train.csv")
+        train_data_df.to_json("train"+str(l1[l])+".json")
     
-    
+        label_df = pd.DataFrame({"label":all_states.unique()})
+        label_df.to_json("label.json")
     
     
     
---
+++ pythonFile/test_naive_bayes.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 10 11:40:25 2018
+
+@author: msarkar
+"""
+
+
+import pandas as pd
+import numpy as np
+
+
+def gaussian(x, mu, sig):
+    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
+
+def classify(sample, parameter):
+    keys = parameter.keys()
+    correct_label = sample["state"]
+    sample_keys = list(sample.keys())
+    sample_keys.remove("state")
+    n = keys.shape[0]
+    m = len(sample_keys)
+    prb = []
+    for i in range(n):
+        ky1 = keys[i]
+        p = 1
+        for j in range(m):
+            ky2 = sample_keys[j]
+            mu = parameter[ky1]["mu"][ky2]
+            sig = parameter[ky1]["sig"][ky2]
+            x = sample[ky2]
+            px = gaussian(x,mu,sig)
+            p *= px
+        prb += [p]
+    if keys[prb.index(max(prb))] == correct_label:
+        return 1
+    else:
+        return 0
+    
+            
+
+if __name__ == '__main__':
+    l=1
+    l1 = list(np.linspace(1,7,7,dtype=np.int)) + list(np.linspace(10,22,13,dtype = np.int))
+    ll = len(l1)
+    for l in range(ll):
+        file_name = 'train' + str(l1[l]) + '.json'
+        train_data_df = pd.read_json(file_name)
+        train_data_df = train_data_df.dropna()
+        #label_df = pd.read_json('label.json')
+        parameter_df = pd.read_json('parameter.json')
+    
+        n = train_data_df.shape[0]
+        #m = label_df.shape[0]
+        correct = 0
+        for i in range(n):
+            sample_df = train_data_df.iloc[i]
+            correct += classify(sample_df, parameter_df)
+        
+        #print(n,correct)
+        correct_percent = round(correct*100/n,2)
+        print('test Data '+ str(l1[l]) + ': '+ str(correct_percent)+'%')
